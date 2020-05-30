593ec43c285de5626273787e99e7d8c07e579d82
jpisano99 <jpisano@cisco.com>
2020-05-30 13:13:58 -0400

    Update

--- my_app/small_med_large_deals.py
+++ my_app/small_med_large_deals.py
@@ -25,20 +25,16 @@ class Customer:
         # self.subs = []  # Simple list of lists of subscriptions[]
         # self.saas = {}
 
-    def add_prod_booking(self, sku, booking_amt):
-        # as_pid_info is a full list of tuples (as_pid, as_cust_name, as_sku)
-        # with ALL PIDs for this SO for this cust_id
-        self.prod_booking.append([sku, booking_amt])
+    def add_prod_booking(self, fiscal_year, fiscal_qtr, sku, booking_amt):
+        self.prod_booking.append([fiscal_year, fiscal_qtr, sku, booking_amt])
 
-    def add_svc_booking(self, sku, booking_amt):
-        # as_pid_info is a full list of tuples (as_pid, as_cust_name, as_sku)
-        # with ALL PIDs for this SO for this cust_id
-        self.svc_booking.append([sku, booking_amt])
+    def add_svc_booking(self, fiscal_year, fiscal_qtr, sku, booking_amt, svc_type):
+        self.svc_booking.append([fiscal_year, fiscal_qtr, sku, booking_amt, svc_type])
 
-    def add_sls_levels(self, sls_lv1, sls_lv2):
+    def add_sls_levels(self, sls_lv1, sls_lv2, sls_lv3):
         # as_pid_info is a full list of tuples (as_pid, as_cust_name, as_sku)
         # with ALL PIDs for this SO for this cust_id
-        self.segments.append([sls_lv1, sls_lv2])
+        self.segments.append([sls_lv1, sls_lv2, sls_lv3])
 
     def add_order(self, order_num, sku):
         # Check to see if this SO # already exists
@@ -69,6 +65,30 @@ class Customer:
         self.subs.append(sub_info)
 
 
+def create_summary(total_prod, total_svc):
+    deal_size = ''
+    if total_prod <= 100000:
+        deal_size = "Small"
+    elif (total_prod > 100000) and (total_prod <= 750000):
+        deal_size = 'Medium'
+    elif total_prod > 750000:
+        deal_size = 'Large'
+
+    if total_prod > 0:
+        prod_to_svc_ratio = round(((total_svc/total_prod)*100), 2)
+        prod_to_svc_ratio = str(prod_to_svc_ratio)+' %'
+    else:
+        prod_to_svc_ratio = ''
+
+    # results = {'cust_key': cust_key,
+    #            'deal_size': deal_size,
+    #            'total_prod': total_prod,
+    #            'total_svc': total_svc,
+    #            'prod_to_svc_mix': prod_to_svc_ratio
+    #            }
+    return [deal_size, prod_to_svc_ratio]
+
+
 if __name__ == "__main__" and __package__ is None:
     wb_prod, ws_prod = open_wb('all_prod_tetration_bookings.xlsx')
     wb_svc, ws_svc = open_wb('all_as_tetration_bookings.xlsx')
@@ -83,8 +103,11 @@ if __name__ == "__main__" and __package__ is None:
     # Loop over the product sheet
     #
     for row in range(1, ws_prod.nrows):
+        fiscal_year = ws_prod.cell_value(row, 0)
+        fiscal_qtr = ws_prod.cell_value(row, 1)
         sls_lv1 = ws_prod.cell_value(row, 3)
         sls_lv2 = ws_prod.cell_value(row, 4)
+        sls_lv3 = ws_prod.cell_value(row, 5)
         cust_alias = ws_prod.cell_value(row, 13)
         cust_id = ws_prod.cell_value(row, 15)
         sku = ws_prod.cell_value(row, 19)
@@ -98,16 +121,20 @@ if __name__ == "__main__" and __package__ is None:
             cust_db[cust_id] = cust_obj
 
         cust_obj.add_alias(cust_alias)
-        cust_obj.add_prod_booking(sku, prod_booking)
-        cust_obj.add_sls_levels(sls_lv1, sls_lv2)
+        cust_obj.add_prod_booking(fiscal_year, fiscal_qtr, sku, prod_booking)
+        cust_obj.add_sls_levels(sls_lv1, sls_lv2, sls_lv3)
 
     #
     # Loop over the services sheet
     #
     for row in range(1, ws_svc.nrows):
+        fiscal_year = ws_prod.cell_value(row, 0)
+        fiscal_qtr = ws_prod.cell_value(row, 1)
+        svc_type = ws_svc.cell_value(row, 3)
+        sku = ws_svc.cell_value(row, 7)
         sls_lv1 = ws_svc.cell_value(row, 11)
         sls_lv2 = ws_svc.cell_value(row, 12)
-        sku = ws_svc.cell_value(row, 7)
+        sls_lv3 = ws_svc.cell_value(row, 13)
         cust_alias = ws_svc.cell_value(row, 20)
         cust_id = ws_svc.cell_value(row, 21)
         svc_booking = ws_svc.cell_value(row, 23)
@@ -120,48 +147,99 @@ if __name__ == "__main__" and __package__ is None:
             cust_db[cust_id] = cust_obj
 
         cust_obj.add_alias(cust_alias)
-        cust_obj.add_svc_booking(sku, svc_booking)
-        cust_obj.add_sls_levels(sls_lv1, sls_lv2)
+        cust_obj.add_svc_booking(fiscal_year, fiscal_qtr, sku, svc_booking, svc_type)
+        cust_obj.add_sls_levels(sls_lv1, sls_lv2, sls_lv3)
 
     print('There are ', len(cust_db), 'Unique Customer IDs')
     print()
 
-    # for cust_id, cust_obj in cust_db.items():
-    #     print(cust_id, '\t', cust_obj.aliases)
-    #     print('\t\t', cust_obj.prod_booking)
-    #     print('\t\t', cust_obj.svc_booking)
-    #     print('\t\t', cust_obj.segments)
-    #     print()
+    #
+    # Output the Customer Objects to xlsx
+    #
 
     output_list = []
-    header_row = ['customer_id', 'customer_aliases',
-                  'sales_level_1', 'sales_level_2', 'sku_type',
-                  'prod_sku', 'prod_booking',
-                  'svc_sku', 'svc_booking']
+    header_row = ['customer_id', 'customer_aliases', 'customer_id_aliases',
+                  'fiscal_year', 'fiscal_qtr',
+                  'sales_level_1', 'sales_level_2', 'sales_level_3',
+                  'sku_type', 'service_type',
+                  'prod_sku', 'prod_booking', 'service_booking']
     output_list.append(header_row)
 
+    summary_list = []
+    header_row = ['customer_id_aliases',
+                  'fiscal_year', 'fiscal_qtr',
+                  'sales_level_1', 'sales_level_2', 'sales_level_3',
+                  'deal_size', 'prod_to_svc_ratio',
+                  'total_prod_booking', 'total_svc_booking']
+    summary_list.append(header_row)
+
+    # Loop over all the Customer Objects
     for cust_id, cust_obj in cust_db.items():
 
+        # Format the aliases for output
         aliases = ''
         for alias in cust_obj.aliases:
             aliases = alias + " : " + aliases
+        aliases = aliases[:-3]
 
-        sls_lv1 = cust_obj.segments[0][0]
-        sls_lv2 = cust_obj.segments[0][1]
-        print(aliases)
+        # Find the best fit for sales level
+        tmp_dict = {}
+        for segment in cust_obj.segments:
+            tmp_tuple = tuple(segment)
+            if tmp_tuple in tmp_dict:
+                tmp_dict[tmp_tuple] = tmp_dict[tmp_tuple] + 1
+            else:
+                tmp_dict[tmp_tuple] = 1
 
+        # Use the most frequently occurring Sales Levels
+        most_freq = 0
+        best_segment = ''
+        for segment, freq in tmp_dict.items():
+            if freq >= most_freq:
+                best_segment = segment
+
+        sls_lv1 = best_segment[0]
+        sls_lv2 = best_segment[1]
+        sls_lv3 = best_segment[2]
+
+        # Create a key of the cust_id & aliases
+        cust_key = cust_id + ' - ' + aliases
+        cust_key = cust_key[:50]
+
+        deal_size = ''
+        total_prod = 0
         for booking in cust_obj.prod_booking:
-            prod_sku = booking[0]
-            prod_rev = booking[1]
-            output_list.append([cust_id, aliases, sls_lv1, sls_lv2, 'product', prod_sku, prod_rev, '', 0])
-            # print(booking)
+            fiscal_year = booking[0]
+            fiscal_qtr = booking[1]
+            prod_sku = booking[2]
+            prod_rev = booking[3]
+            total_prod = total_prod + prod_rev
+            output_list.append([cust_id, aliases, cust_key, fiscal_year, fiscal_qtr,
+                                sls_lv1, sls_lv2, sls_lv3,
+                                'product', '', prod_sku, prod_rev, 0])
+
+        total_svc = 0
 
         for booking in cust_obj.svc_booking:
-            svc_sku = booking[0]
-            svc_rev = booking[1]
-            output_list.append([cust_id, aliases, sls_lv1, sls_lv2, 'service', svc_sku, 0, '', svc_rev])
-            # print(booking)
+            fiscal_year = booking[0]
+            fiscal_qtr = booking[1]
+            svc_sku = booking[2]
+            svc_rev = booking[3]
+            svc_type = booking[4]
+            total_svc = total_svc + svc_rev
+            output_list.append([cust_id, aliases, cust_key, fiscal_year, fiscal_qtr,
+                                sls_lv1, sls_lv2, sls_lv3,
+                                'service', svc_type, svc_sku, 0, svc_rev])
 
-    push_list_to_xls(output_list, 'stan.xlsx')
+        my_summary = create_summary(total_prod, total_svc)
+        summary_list.append([cust_key, fiscal_year, fiscal_qtr,
+                        sls_lv1, sls_lv2, sls_lv3,
+                        my_summary[0], my_summary[1], total_prod, total_svc])
+
+        # print(summary_list)
+        # time.sleep(.4)
+
+    push_list_to_xls(output_list, 'detail.xlsx')
+    push_list_to_xls(summary_list, 'summary.xlsx')
     exit()
 
